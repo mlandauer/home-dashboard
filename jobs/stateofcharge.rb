23b6c6e729e6e8d400ea4877582e3215c71e72e7
Matthew Landauer <mlandauer@gmail.com>
2020-03-17 14:28:00 +1100

    Add load and charge to display

--- jobs/stateofcharge.rb
+++ jobs/stateofcharge.rb
@@ -11,5 +11,15 @@ SCHEDULER.every '5s' do
   query = "from(bucket: \"#{bucket}\") |> range(start: -2h) |> filter(fn: (r) => r._measurement == \"solar\") |> filter(fn: (r) => r._field == \"soc\") |> last()"
   tables = client.create_query_api.query(query: query, org: org)
 
-  send_event('stateofcharge',   { value: tables[0].records[0].value })
+  send_event('stateofcharge', { value: tables[0].records[0].value })
+
+  query = "from(bucket: \"#{bucket}\") |> range(start: -2h) |> filter(fn: (r) => r._measurement == \"solar\") |> filter(fn: (r) => r._field == \"load\") |> last()"
+  tables = client.create_query_api.query(query: query, org: org)
+
+  send_event('load', { current: tables[0].records[0].value.round(1) })
+
+  query = "from(bucket: \"#{bucket}\") |> range(start: -2h) |> filter(fn: (r) => r._measurement == \"solar\") |> filter(fn: (r) => r._field == \"charge\") |> last()"
+  tables = client.create_query_api.query(query: query, org: org)
+
+  send_event('charge', { current: tables[0].records[0].value.round(1) })
 end
