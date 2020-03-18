8081ad10d5943f9b1ed6cb4b5da36a3fba43a577
Matthew Landauer <mlandauer@gmail.com>
2020-03-19 06:43:17 +1100

    Get the values in one query

--- jobs/solar.rb
+++ jobs/solar.rb
@@ -7,19 +7,15 @@ client = InfluxDB2::Client.new(
 bucket = ENV["INFLUXDB_BUCKET"]
 org = ENV["INFLUXDB_ORG"]
 
+def value(tables, field)
+  tables.find {|i, t| t.records[0].field == field}[1].records[0].value
+end
+
 SCHEDULER.every '5s' do
-  query = "from(bucket: \"#{bucket}\") |> range(start: -2h) |> filter(fn: (r) => r._measurement == \"solar\") |> filter(fn: (r) => r._field == \"soc\") |> last()"
+  query = "from(bucket: \"#{bucket}\") |> range(start: -2h) |> filter(fn: (r) => r._measurement == \"solar\") |> last()"
   tables = client.create_query_api.query(query: query, org: org)
 
-  send_event('stateofcharge', { value: tables[0].records[0].value })
-
-  query = "from(bucket: \"#{bucket}\") |> range(start: -2h) |> filter(fn: (r) => r._measurement == \"solar\") |> filter(fn: (r) => r._field == \"load\") |> last()"
-  tables = client.create_query_api.query(query: query, org: org)
-
-  send_event('load', { current: tables[0].records[0].value.round(1) })
-
-  query = "from(bucket: \"#{bucket}\") |> range(start: -2h) |> filter(fn: (r) => r._measurement == \"solar\") |> filter(fn: (r) => r._field == \"charge\") |> last()"
-  tables = client.create_query_api.query(query: query, org: org)
-
-  send_event('charge', { current: tables[0].records[0].value.round(1) })
+  send_event('stateofcharge', { value: value(tables, "soc") })
+  send_event('load', { current: value(tables, "load").round(1) })
+  send_event('charge', { current: value(tables, "charge").round(1) })
 end
